dcf78d694d6032e7fe35c6354a159c6f9ff3cd53
syntakks <syntakks@gmail.com>
2020-02-02 23:50:43 -0600

    Splash and Start scenes added.
    SceneLoader added.
    Splash screen with delay.
    Start screen.
    TextMeshPro imported.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex; 
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScene()); 
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(3.0f); 
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameOverScene()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
